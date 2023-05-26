d37dfc20cb2576cca52bc02dc2e7092346d33677
whatasame <whatasame@gmail.com>
2023-05-26 09:39:12 +0900

    Initial commit

---
+++ src/main/java/com/aid/aidbackend/AidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aid.aidbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidBackendApplication.class, args);
+    }
+
+}
