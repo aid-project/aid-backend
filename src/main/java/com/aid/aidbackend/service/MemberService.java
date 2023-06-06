48bc4710c69cde6b2d5a3ceda8a776396f9734a6
whatasame <whatasame@gmail.com>
2023-06-06 19:52:57 +0900

    feat: 회원 데이터 중복 확인 기능

--- src/main/java/com/aid/aidbackend/service/MemberService.java
+++ src/main/java/com/aid/aidbackend/service/MemberService.java
@@ -2,6 +2,7 @@ package com.aid.aidbackend.service;
 
 import com.aid.aidbackend.dto.MemberDto;
 import com.aid.aidbackend.entity.Member;
+import com.aid.aidbackend.exception.DuplicateMemberException;
 import com.aid.aidbackend.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,9 @@ public class MemberService {
     }
 
     public Member join(MemberDto memberDto) {
+        validateDuplicateEmail(memberDto.email());
+        validateDuplicateNickname(memberDto.nickname());
+
         Member member = Member.builder()
                 .email(memberDto.email())
                 .password(memberDto.password())
@@ -22,5 +26,17 @@ public class MemberService {
                 .build();
 
         return memberRepository.save(member);
+    }
+
+    private void validateDuplicateEmail(String email) {
+        if (memberRepository.findByEmail(email).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 존재하는 이메일입니다.");
+        }
+    }
+
+    private void validateDuplicateNickname(String nickname) {
+        if (memberRepository.findByNickname(nickname).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 존재하는 닉네임입니다.");
+        }
     }
 }
