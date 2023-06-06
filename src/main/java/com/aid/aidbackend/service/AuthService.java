0e61c175fcc9481663b13a417446ff71fc1537c7
whatasame <whatasame@gmail.com>
2023-06-07 01:41:11 +0900

    feat: 이메일, 비밀번호 검증
    < AuthService.login() 내부 동작 과정 >
    1. AuthenticationManagerBuilder에서 getObject()를 호출한다.
    2. AuthenticationManager의 구현체인 ProviderManager를 가져온다.
    3. ProviderManager.authenticate()를 호출한다.
    4. 3번 메서드 안에서 ProviderManager가 가지고 있는 List<AuthenticationProvider>들의 authenticate()를 호출한다.
    5. 4번은 AuthenticationProvider의 구현체인 추상 클래스 AbstractUserDetailsAuthenticationProvider.authenticate()를 호출한다.
    6. 5번 메서드 안에서 this.retrieveUser()를 호출하지만 이는 추상 메서드이다.
    7. AbstractUserDetailsAuthenticationProvider를 상속한 DaoAuthenticationProvider가 구현한 retrieveUser()를 호출한다.
    8. 7번 메서드 내에서 DaoAuthenticationProvider가 가지고 있는 UserDetailsService의 loadUserByUsername()을 호출한다.
    
    이때, 8번에서 호출한 UserDetailsService가 현재 클래스다. 현재 클래스가 UserDetailService 인터페이스를 구현하여 Spring Bean으로 등록했기 때문이다.
    
    Q. UserDetailsService를 구현하여 Spring Bean으로 등록했는데 언제 주입되나?
    A. Spring Bean으로 등록한 현재 클래스는 DaoAuthenticationProvider에 주입된다. 이때, 생성자 주입이 아니라 InitializeUserDetailBeanMangerConfigurer.configue()에서 DaoAuthenticationProvider의 setter를 사용하여 주입(수정자 주입)된다. 추가로 Spring Bean에 등록되어 있는 PasswordEncoder가 있다면 동일한 방식으로 DaoAuthenticationProvider에 주입된다.
    
    Q. AuthenticationManagerBuilder는 Spring Bean으로 언제 등록되길래 현재 클래스에서 주입받을 수 있나?
    A. AuthenticationConfiguration에서 Spring Bean으로 등록한다.
    
    Q. AuthenticationManagerBuilder에서 getObject()를 하면 ProviderManager가 반환되는 이유?
    
    Q. ProviderManager가 필드로 providers를 가지고 있는데 DaoProviderManager는 언제 providers에 추가되나?

---
+++ src/main/java/com/aid/aidbackend/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.aid.aidbackend.service;
+
+import com.aid.aidbackend.controller.dto.TokenDto;
+import com.aid.aidbackend.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtProvider jwtProvider;
+
+    public TokenDto login(String email, String password) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(email, password);
+
+        /* 이메일, 비밀번호 검증 */
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        return jwtProvider.generateTokenDto(authentication);
+    }
+}
