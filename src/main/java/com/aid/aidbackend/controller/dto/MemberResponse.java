375eddafba4cc4b989d5e204089d338cd513d47c
whatasame <whatasame@gmail.com>
2023-06-08 00:48:41 +0900

    fix: JSON 형식을 snake_case으로 변경

--- src/main/java/com/aid/aidbackend/controller/dto/MemberResponse.java
+++ src/main/java/com/aid/aidbackend/controller/dto/MemberResponse.java
@@ -1,9 +1,12 @@
 package com.aid.aidbackend.controller.dto;
 
 import com.aid.aidbackend.entity.Member;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import java.time.LocalDateTime;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MemberResponse(
         String email,
         String nickname,
